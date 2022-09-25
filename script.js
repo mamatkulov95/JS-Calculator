44bf6f78ead1d469058b684fe9c080cddc96c512
M_Coder <m.mamatkulov95@gmail.com>
2022-09-25 10:54:25 +0500

    JS practice

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let names = ["Akmal", "Sobirjon", "Abdulloh", "Abdurahmon", "Abu Bakr Siddiq", "Umar"]
+
+// for (let i = 0; i < names.length; i++){
+//     console.log("-" + names[i]);
+// }
+
+function random(){
+    console.log(names[0])
+}
+
+random();
